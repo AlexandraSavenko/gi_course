8166974d73409d20fe2b453410841054d8dcf98f
Sasha Savenko <sashasavenko3@gmail.com>
2026-04-01 20:54:40 +0300

    wordBank

--- src/components/taskOne/TaskOne.jsx
+++ src/components/taskOne/TaskOne.jsx
@@ -1,27 +1,46 @@
-import css from './TaskOne.module.css'
+import { useMemo, useState } from "react";
+import css from "./TaskOne.module.css";
 
 const TaskOne = () => {
-  const word = "consider"
-  const wordArray = word.split("")
-  console.log(wordArray)
-  const mixed = wordArray.sort()
-  console.log(mixed)
+  const [selected, setSelected] = useState("");
+  const words = ["consider", "enjoy", "finish"];
+
+  const wordArray = useMemo(() => {
+    return selected ? selected.split("") : [];
+  }, [selected]);
+  const mixed = useMemo(() => {
+    return [...wordArray].sort();
+  }, [wordArray]);
+
   return (
     <div className={css.wrap}>
       <ul className={css.circle}>
-        {
-          mixed.map((el, index) => <li className={css.letter} key={index} style={{"--i": index, "--total": mixed.length}}>{el}</li> )
-        }
+        {mixed.map((el, index) => (
+          <li
+            className={css.letter}
+            key={index}
+            style={{ "--i": index, "--total": mixed.length }}
+          >
+            {el}
+          </li>
+        ))}
       </ul>
       <ul className={css.list}>
-        {
-          wordArray.map(el => <li className={css.gap} key={el}>
+        {wordArray.map((el, index) => (
+          <li className={css.gap} key={index}>
             <div className={css.box}></div>
-          </li> )
-        }
+          </li>
+        ))}
+      </ul>
+      <ul className={css.wordBank}>
+        {words.map((el, index) => (
+          <li className={css.word} key={index} onClick={() => setSelected(el)}>
+            {el.split("").sort()}
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default TaskOne;
